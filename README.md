9ce4d610293f58ffc0f714cc5583668e57609316
Danny Doan <dannykdoan@gmail.com>
2023-07-31 17:27:18 -0700

    prevent some unnecessary fetches; README

--- README.md
+++ README.md
@@ -1,52 +1,15 @@
-# Next.js Starter Template with built in Layout components
+# zotsearch
 
-This is a template for rapidly starting Next.js TypeScript projects with built in components relating to page layout. Inspired by [@clxmente's template](https://github.com/clxmente/next) with a few added dependencies + a components/layout folder that holds components including a Navbar and Footer as well as a general Layout component that wraps all the pages.
+A course discovery platform allowing UCI students to search for classes using natural language.
 
-## What you'll get (a fully formatted page with a responsive navigation bar and footer)
+## Tech Stack
 
-[Here's the site this template will start off with.](https://next-template-dannykd.vercel.app/)
+**Client:** Next.js, React, TailwindCSS
 
-<a href="https://ibb.co/RDbqcvQ"><img width=500 src="https://i.ibb.co/vjqSkwL/Screen-Shot-2022-10-14-at-10-53-00-PM.png" alt="Screen-Shot-2022-10-14-at-10-53-00-PM" border="0"></a>
+**Server:** FastAPI, AWS Lambda, PineconeDB
 
-<a href="https://ibb.co/CspHWZP"><img width=300 src="https://i.ibb.co/BKHCwxn/Screen-Shot-2022-10-15-at-5-09-43-AM.png" alt="Screen-Shot-2022-10-15-at-5-09-43-AM" border="0"></a>
-
-
-
-## Dependencies Included
-
-[Next.js](https://nextjs.org/)
-
-[TailwindCSS](https://tailwindcss.com/)
-
-[Prettier](https://prettier.io/)
-
-[@headlessui/react](https://headlessui.com/)
-
-[@heroicons/react](https://heroicons.com/)
-
-## Instructions
-
-1. Start a new Next project following this template with your choice of package manager (yarn/npm/pnpm(recommended)).
-
-       pnpm create next-app YOUR_PROJECT_NAME -e https://github.com/dannykd/next-template
-
-       npm create next-app YOUR_PROJECT_NAME -e https://github.com/dannykd/next-template
-
-       yarn create next-app YOUR_PROJECT_NAME -e https://github.com/dannykd/next-template
-
-2. Install dependencies.
-       
-       pnpm install
-
-3. Change placeholder strings. If you CRTL-F "INSERT" you should see all the string that should be changed.
+**External APIs:** PeterPortal, OpenAI, Pinecone
 
 
-        <Link href="/INSERT_PAGE_PATH">INSERT_PAGE_NAME</Link> 
-        
-        Should be changed to ⤵️
-        
-        <Link href="/home">Home</Link>
-        
-        List of strings to change/remove: **INSERT_APP_NAME** (2x), **INSERT_PAGE_NAME** (4x), **INSERT_PAGE_PATH** (4x) in components/layout
-
-3. Delete the **INSERT_PAGE_PATH.tsx** page or use it as a Page template and your project should be ready for your own code!
+## Embeddings
+It's magic but basically if you have a string of text, you can get an embedding from it using some ML model (in our case text-embedding-ada-002 from OpenAI). An embedding is a vector of floating point numbers which (somehow) represents the semantic meaning of the text. So if you have a database of embeddings, you can see which strings are semantically similar to each other by comparing the distance of their embeddings, the closer being more semantically similar. Embeddings are the core concept that allows zotsearch to function
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,6 @@ import Card  from "../components/Card"
 import Pagination from "../components/Pagination";
 import Loader from "../components/Loader";
 
-
 interface Course {
   id: string,
   desc: string,
@@ -22,15 +21,16 @@ const Home: NextPage = () => {
   const [courses, setCourses] = useState<Course[]>([])
   const [currentPage, setCurrentPage] = useState<number>(1)
   const [postsPerPage, setPostsPerPage] = useState<number>(4)
+  const [lastQuery, setLastQuery] = useState<string>('')
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value);
   };
 
   const handleClick = async () => {
-    // Do something when the button is clicked
-    if (text == "") { toast.error("Must have atleast one word", {duration: 2500}); return;}
+    if (text.trim() == "") { toast.error("Must have atleast one word", {duration: 2500}); return;}
     if (loading) { toast.error("Please wait until your request has finished loading", {duration: 2500}); return;}
+    if (lastQuery.trim() == text.trim()) {return;}
     setLoading(true)
     const request = {
       method: 'POST',
@@ -51,6 +51,7 @@ const Home: NextPage = () => {
     catch (err : any) {
       toast.error(err.message, {duration: 2500})
     }
+    setLastQuery(text)
     setLoading(false)
   };
 
@@ -64,11 +65,9 @@ const Home: NextPage = () => {
   const firstPostIndex = lastPostIndex - postsPerPage;
   const currentCourses = courses.slice(firstPostIndex, lastPostIndex)
   
-
-
   return (
     <div className="mx-auto max-w-xl">
-      <h1 className="mt-8 md:mt-14 flex justify-center text-center text-2xl md:text-3xl font-semibold">Effortless UCI course discovery using natural language 💬</h1>
+      <h1 className="mt-8 md:mt-14 flex justify-center text-center text-2xl md:text-3xl font-semibold">Effortlessly discover UCI courses using natural language 💬</h1>
       <div className="bg-black rounded-lg p-2 mt-6 ring-1 ring-neutral-600 flex">
         <input onChange={handleChange} onKeyUp={handleKeyPress} className="bg-inherit sm:text-xl px-2 w-full outline-none" placeholder="Ex. Marginalized communities in the US" maxLength={100}></input>
         <button onClick={handleClick} className="hover:scale-110 px-1 transition ease-in-out">
@@ -79,17 +78,15 @@ const Home: NextPage = () => {
       <h1 className="text-center mt-10 text-xl font-semibold">Recommended Courses 📚</h1>
       <div className="bg-black h-[545px] mt-2 rounded-lg ring-1 ring-neutral-600 p-3 sm:p-6 py-6 space-y-4 items-center">
         {
-          loading == false && courses.length == 0 ? <div className="w-full h-full mx-auto flex justify-center items-center text-neutral-500">search something to get some results :)</div> : <></>
+          loading == false && courses.length == 0 ? <div className="w-full h-full mx-auto flex justify-center items-center text-neutral-500">Search something to get recommendations!</div> : <></>
         }
         { loading ? <Loader></Loader> :
           currentCourses.map((course, index) => {
             return <Card key={course.id} id={course.id} title={course.title} desc={course.desc}/>
           })
         }
-       
       </div>
       <Pagination totalPosts={courses.length} postsPerPage={postsPerPage} setCurrentPage={setCurrentPage} currentPage={currentPage}/>
-      
     </div>
   );
 };
