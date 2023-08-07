4315e36a403963aba0a6af4193a83e95b7d3a1de
Danny Doan <dannykdoan@gmail.com>
2023-08-06 20:32:32 -0700

    darker card color

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 const Card: FC<CardProps> = ({title, desc, id}) => {
   return (
-    <div className="max-h-28 overflow-hidden text-base dark:text-white dark:bg-neutral-900 bg-neutral-300 ring ring-[0.5px] ring-neutral-500 text-black w-full rounded-md px-2 py-1">
+    <div className="max-h-28 overflow-hidden text-base dark:text-white dark:bg-[#0F0F10] bg-neutral-300 ring ring-[0.5px] ring-neutral-500 text-black w-full rounded-md px-2 py-1">
       <div className="h-28 overflow-y-auto p-2">
         <h1><span className="font-semibold">{id}:</span> <span>{title}</span></h1>
         <p className="text-neutral-700 dark:text-neutral-400">{desc}</p>
